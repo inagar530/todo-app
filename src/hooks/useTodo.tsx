a412bdb074b98cb876e19ceef2c59f3d5ce9de9c
inagar530 <122870238+inagar530@users.noreply.github.com>
2025-09-25 15:55:50 +0530

    create useTodo file
    "Implemented a Todo hook with Add Todo functionality and persistent storage using localStorage."

---
+++ src/hooks/useTodo.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+  priority: "low" | "medium" | "high";
+  category:
+    | "general"
+    | "work"
+    | "personal"
+    | "shopping"
+    | "health"
+    | "learning";
+  dueDate: number | null;
+  createdAt: string;
+  notes: string | "";
+}
+
+export type NewTodo = Omit<Todo, "id" | "completed" | "createdAt">;
+export function useTodos() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // Load from localStroage
+  useEffect(() => {
+    const saved = localStorage.getItem("todos");
+    if (saved) setTodos(JSON.parse(saved));
+  }, []);
+
+  //  Saved to localStroage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  //   Add new todo
+  const addtodo = (newTodo: NewTodo) => {
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now(),
+        text: newTodo.text,
+        completed: false,
+        priority: newTodo.priority || "medium",
+        category: newTodo.category || "general",
+        dueDate: newTodo.dueDate || null,
+        createdAt: new Date().toISOString(),
+        notes: newTodo.notes || "",
+      },
+    ]);
+  };
+  return {
+    addtodo,
+  };
+}
